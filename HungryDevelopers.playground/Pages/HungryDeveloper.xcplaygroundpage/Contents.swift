a45f8e21cdb74bbe3ddab4b1d43774edf1696cdd
Dongwoo Pae <dongwoopae@gmail.com>
2019-07-31 22:22:09 -0700

    finished MVP

--- HungryDevelopers.playground/Pages/HungryDeveloper.xcplaygroundpage/Contents.swift
+++ HungryDevelopers.playground/Pages/HungryDeveloper.xcplaygroundpage/Contents.swift
@@ -3,21 +3,21 @@ import Foundation
 class Spoon {
     
     private var lock = NSLock()
-    var isBeingUsed : Bool
+    //var isBeingUsed : Bool
     var index: Int
     
     func pickUp() {
         lock.lock()
-        isBeingUsed = true
+        //isBeingUsed = true
     }
     
     func putDown() {
-        isBeingUsed = false
+        //isBeingUsed = false
         lock.unlock()
     }
     
-    init(index: Int, isBeingUsed: Bool = false) {
-        self.isBeingUsed = isBeingUsed
+    init(index: Int) {
+       // self.isBeingUsed = isBeingUsed
         self.index = index
     }
     
@@ -37,48 +37,61 @@ class Developer {
     
     func think() {
         
-//        if leftSpoon.index < rightSpoon.index {
-//            leftSpoon.pickUp()
-//            print("\(name) picked up left spoon")
-//            rightSpoon.pickUp()
-//            print("\(name) picked up right spoon")
-//        } else {
-//            rightSpoon.pickUp()
-//            print("\(name) picked up right spoon")
-//            leftSpoon.pickUp()
-//            print("\(name) picked up left spoon")
-//        }
-        
-        
-        if rightSpoon.isBeingUsed == false {
-        rightSpoon.pickUp()
-        print("\(name) picking up right spoon")
-        }
-        
-        if leftSpoon.isBeingUsed == false {
-        leftSpoon.pickUp()
-        print("\(name) picking up left spoon")
+            print("\(name) is thinking")
+        if leftSpoon.index < rightSpoon.index {
+            leftSpoon.pickUp()
+            print("\(name) picked up left spoon")
+            rightSpoon.pickUp()
+            print("\(name) picked up right spoon")
+        } else {
+            rightSpoon.pickUp()
+            print("\(name) picked up right spoon")
+            leftSpoon.pickUp()
+            print("\(name) picked up left spoon")
         }
     }
     
+        
+//        if rightSpoon.isBeingUsed == false {
+//        rightSpoon.pickUp()
+//        print("\(name) picking up right spoon")
+//        }
+//
+//        if leftSpoon.isBeingUsed == false {
+//        leftSpoon.pickUp()
+//        print("\(name) picking up left spoon")
+//        }
+//    }
+
     
     func eat() {
         //putdown right and left
         //let randomTime = UInt32.random(in: 0xD800...0xDFFF)
+        print("\(name) is eating")
+        
         usleep(1000)
-        if leftSpoon.isBeingUsed == true {
         leftSpoon.putDown()
         print("\(name) put down left spoon")
-        }
         usleep(1000)
-        if rightSpoon.isBeingUsed == true {
         rightSpoon.putDown()
         print("\(name) put down right spoon")
-        }
+
+    
+//        usleep(1000)
+//        if leftSpoon.isBeingUsed == true {
+//        leftSpoon.putDown()
+//        print("\(name) put down left spoon")
+//        }
+//        usleep(1000)
+//        if rightSpoon.isBeingUsed == true {
+//        rightSpoon.putDown()
+//        print("\(name) put down right spoon")
+//        }
     }
     
     func run() {
         while(true) {
+            print("while loop starts----------------------------------------")
             think()
             eat()
             return
